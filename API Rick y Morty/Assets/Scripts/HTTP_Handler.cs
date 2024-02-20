a28c29a9a194b362384e38b77febcd28a2c41b35
mcaladp09 <theredstonefighter@gmail.com>
2024-02-20 14:04:46 -0500

    Finishing Touch

--- API Rick y Morty/Assets/Scripts/HTTP_Handler.cs
+++ API Rick y Morty/Assets/Scripts/HTTP_Handler.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
 
 public class HTTP_Handler : MonoBehaviour
 {
     private string fakeApiUrl = "https://my-json-server.typicode.com/mcaladp09/Actividad_Rick_y_Morty_API";
-    private string RickAndMoryApiUrl = "https://rickandmortyapi.com/api";
-    private int imagesDownloadsCounter;//
+    private string RickyMortyAPIURL = "https://rickandmortyapi.com/api";
+    private int countImages;
 
-    public List<RawImage> cardsImages = new List<RawImage>();//name
-    public List<TextMeshProUGUI> cardsNames = new List<TextMeshProUGUI>();
+    public List<TextMeshProUGUI> cardNamesList = new List<TextMeshProUGUI>();
+    public List<TextMeshProUGUI> cardIDsList = new List<TextMeshProUGUI>();
+    public List<RawImage> cardImagesLists = new List<RawImage>();
 
     Coroutine sendRequest_GetCharacters;
 
     public void SendRequest(int userID)
     {
 
-        imagesDownloadsCounter = 0;
         sendRequest_GetCharacters = StartCoroutine(GetUserData(userID));
 
     }
@@ -37,11 +37,12 @@ public class HTTP_Handler : MonoBehaviour
         {
             if (request.responseCode == 200)
             {
+                countImages = 0;
                 UserData user = JsonUtility.FromJson<UserData>(request.downloadHandler.text);
                 foreach (int cardID in user.deck)
                 {
-                    StartCoroutine(GetCharacters(cardID, imagesDownloadsCounter));
-                    imagesDownloadsCounter++;
+                    StartCoroutine(GetCharacters(cardID, countImages));
+                    countImages++;
                 }
             }
         }
@@ -49,7 +50,7 @@ public class HTTP_Handler : MonoBehaviour
 
     IEnumerator GetCharacters(int cardId, int deckIndex)
     {
-        UnityWebRequest request = UnityWebRequest.Get(RickAndMoryApiUrl + "/character/" + cardId);
+        UnityWebRequest request = UnityWebRequest.Get(RickyMortyAPIURL + "/character/" + cardId);
 
         yield return request.SendWebRequest();
 
@@ -59,12 +60,13 @@ public class HTTP_Handler : MonoBehaviour
         }
         else
         {
-            Debug.Log(request.responseCode);//
+            Debug.Log(request.responseCode);
 
             if (request.responseCode == 200)
             {
                 Character character = JsonUtility.FromJson<Character>(request.downloadHandler.text);
-                cardsNames[deckIndex].text = character.name;
+                cardNamesList[deckIndex].text = character.name;
+                cardIDsList[deckIndex].text = character.id.ToString();
                 StartCoroutine(DownloadImage(character.image, deckIndex));
             }
         }
@@ -81,7 +83,7 @@ public class HTTP_Handler : MonoBehaviour
         }
         else
         {
-            cardsImages[deckIndex].texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            cardImagesLists[deckIndex].texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
         }
     }
 }
@@ -101,4 +103,3 @@ public class Character
     public string name;
     public string image;
 }
-
